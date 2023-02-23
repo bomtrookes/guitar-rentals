3624732a99514ce7f0d3ee459d8a36e048889cd7
Tom Brookes <tom.brookes15@gmail.com>
2023-02-23 16:06:12 +0000

    added more flat columns to the flat model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_155504) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_160051) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "flat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flat_id"], name: "index_bookings_on_flat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -21,6 +32,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_155504) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.integer "guests"
+    t.integer "bedrooms"
+    t.integer "beds"
+    t.integer "bathrooms"
+    t.text "summary"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +54,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_155504) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "flats"
+  add_foreign_key "bookings", "users"
 end
