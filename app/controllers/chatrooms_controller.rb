d23e948fdabc1b6108558c7fbfe31c33e36407be
Tom Brookes <tom.brookes15@gmail.com>
2023-03-28 22:00:07 +0100

    chatroom styling

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,11 +1,11 @@
 class ChatroomsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_chatroom, only: [:show, :destroy]
+  before_action :set_chatrooms, only: [:index, :show]
   before_action :set_user, only: [:index, :show]
   before_action :require_participant, only: [:show]
 
   def index
-    @chatrooms = Chatroom.where('user1_id = ? OR user2_id = ?', current_user.id, current_user.id)
   end
 
   def show
@@ -59,6 +59,14 @@ class ChatroomsController < ApplicationController
 
   def set_chatroom
     @chatroom = Chatroom.find(params[:id])
+  end
+
+  def set_chatrooms
+    @chatrooms = Chatroom.joins(:messages)
+    .select('chatrooms.*, MAX(messages.created_at) AS latest_message_time')
+    .where('user1_id = ? OR user2_id = ?', current_user.id, current_user.id)
+    .group('chatrooms.id')
+    .order('latest_message_time DESC')
   end
 
   def require_participant
